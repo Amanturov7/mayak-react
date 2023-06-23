8784c7efea189409084cc6bfd17ef17f77b7ddce
Amanturov7 <edi.amanturov@gmail.com>
2023-06-23 15:56:48 +0600

    init

---
+++ src/footer.routes.js
@@ -0,0 +1,92 @@
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+// import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import OdnoclassnikiIcon from "@mui/icons-material/Odnoclassniki";
+import InstagramIcon from "@mui/icons-material/Instagram";
+// Material Kit 2 React components
+import MKTypography from "components/MKTypography";
+
+// Images
+import logoCT from "assets/images/logo-ct-dark.png";
+// import { Instagram } from "@mui/icons-material";
+
+const date = new Date().getFullYear();
+
+export default {
+  brand: {
+    name: "Маяк",
+    image: logoCT,
+    route: "/",
+  },
+  socials: [
+    {
+      icon: <FacebookIcon />,
+      link: "https://www.facebook.com/profile.php?id=100093416110637&mibextid=ZbWKwL",
+    },
+    {
+      icon: <TwitterIcon />,
+      link: "https://twitter.com/MayakBishkek?t=OqRrvG6GMfEc1C0uWEOQyw&s=08",
+    },
+    {
+      icon: <OdnoclassnikiIcon />,
+      link: "https://ok.ru/profile/587732091285?utm_campaign=android_share&utm_content=profile",
+    },
+    {
+      icon: <YouTubeIcon />,
+      link: "https://www.youtube.com/channel/UCmS1eU5sckxlFWA6hJ1sQ-A",
+    },
+    {
+      icon: <InstagramIcon />,
+      link: "https://instagram.com/mayak_bish?igshid=MzNlNGNkZWQ4Mg==",
+    },
+  ],
+  menus: [
+    {
+      name: "Компания",
+      items: [
+        { name: "О нас", href: "https://www.creative-tim.com/presentation" },
+        { name: "Новости", href: "https://www.creative-tim.com/templates/free" },
+        { name: "Услуги", href: "https://www.creative-tim.com/templates/premium" },
+      ],
+    },
+    {
+      name: "Ресурсы",
+      items: [
+        { name: "Галерея", href: "https://iradesign.io/" },
+        { name: "Зависимые", href: "https://www.creative-tim.com/bits" },
+        { name: "Престарелые", href: "https://www.creative-tim.com/affiliates/new" },
+      ],
+    },
+    {
+      name: "Контактные данные",
+      items: [
+        { name: "Телефон: 0708 578 877" },
+        { name: "ул: Карадарьинская №52" },
+        { name: "rebcentermayakbishkek@gmail.com" },
+      ],
+    },
+    // {
+    //   name: "legal",
+    //   items: [
+    //     { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
+    //     { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
+    //     { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
+    //   ],
+    // },
+  ],
+  copyright: (
+    <MKTypography variant="button" fontWeight="regular">
+      All rights reserved. Copyright &copy; {date} Маяк Бишкек by{" @Amanturov7"}
+      <MKTypography
+        component="a"
+        href="https://www.creative-tim.com"
+        target="_blank"
+        rel="noreferrer"
+        variant="button"
+        fontWeight="regular"
+      ></MKTypography>
+    </MKTypography>
+  ),
+};
---
+++ src/pages/Presentation/sections/Services.js
@@ -0,0 +1,110 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+// import RotatingCard from "examples/Cards/RotatingCard";
+// import RotatingCardFront from "examples/Cards/RotatingCard/RotatingCardFront";
+// import RotatingCardBack from "examples/Cards/RotatingCard/RotatingCardBack";
+import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
+
+// Images
+// import bgFront from "assets/images/rotating-card-bg-front.jpeg";
+// import bgBack from "assets/images/rotating-card-bg-back.jpeg";
+
+function Services() {
+  return (
+    <MKBox component="section" py={6} my={6}>
+      <Container>
+        <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
+          <Grid item xs={12} lg={4} sx={{ mx: "auto" }}>
+            {/* <RotatingCard>
+              <RotatingCardFront
+                image={bgFront}
+                icon="touch_app"
+                title={
+                  <>
+                    Узнать
+                    <br />
+                    подробнее
+                  </>
+                }
+                description="Свяжитесь с нами чтобы получить бесплатную консультацию."
+              />
+              <RotatingCardBack
+                image={bgBack}
+                title="Услуги"
+                description="Анонимный Реабилитационный центр предоставляет ряд услуг, чтобы узнать больше нажмите 'Подробнее'."
+                action={{
+                  type: "internal",
+                  route: "/sections/page-sections/page-headers",
+                  label: "Подробнее",
+                }}
+              />
+            </RotatingCard> */}
+          </Grid>
+          <Grid item xs={6} lg={20} sx={{ ml: "auto" }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="content_copy"
+                  title="Детокс-капельницы"
+                  description="Это эффективный способ восстановить организм после перенесенных заболеваний и других испытаний. Она ускоряет выздоровление и повышает эффективность дальнейших лечебных мероприятий."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="flip_to_front"
+                  title="Психологические услуги"
+                  description="Психологические услуги - область практического применения психологии, ориентированная на повышение социально-психологической компетентности людей и оказания психологической помощи как отдельному человеку, так и группе или организации"
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={3} sx={{ mt: { xs: 0, md: 6 } }}>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="price_change"
+                  title="Духовные услуги"
+                  description="Духовные услуги - подход, позволяющий улучшить качество жизни пациентов (детей и взрослых) и их семей, столкнувшихся с проблемами, связанными с опасным для жизни заболеванием, путём предотвращения и облегчения страданий за счёт раннего выявления, тщательной оценки и лечения боли и других физических симптомов, а также оказания психосоциальной и духовной поддержки."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="devices"
+                  title="Мотивационные услуги"
+                  description="Проблема поведенческих мотивов является одной из основных в психологии. Особое значение эта проблема приобретает при терапии зависимостей, так как более 50% людей, зависимых от наркотиков и алкоголя, не видят необходимости проходить курс лечения, так как не считают себя больными людьми."
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <DefaultInfoCard
+                  icon="devices"
+                  title="Услуги поддержки в СОЦИО"
+                  description="Услуги поддержки в СОЦИО - помощь гражданину в целях улучшения условий его жизнедеятельности или расширения его возможностей, самостоятельно обеспечивать свои основные жизненные потребности. Социальные услуги оказываются в процессе социальной работы. "
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+export default Services;
