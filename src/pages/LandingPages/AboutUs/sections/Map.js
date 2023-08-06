c2e417733ab4ee0e5b97f6d6dca209fb860884ec
Amanturov7 <edi.amanturov@gmail.com>
2023-08-06 16:36:40 +0600

    mobile version map

--- src/pages/LandingPages/AboutUs/sections/Map.js
+++ src/pages/LandingPages/AboutUs/sections/Map.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet"; // Импорт объекта Icon из библиотеки Leaflet
+import { Icon } from "leaflet";
 import markerIcon from "assets/images/icons/marker-icon.png";
 import markerShadow from "assets/images/icons/marker-shadow.png";
 import PropTypes from "prop-types";
@@ -9,23 +9,33 @@ import PropTypes from "prop-types";
 function MapWithMarker({ lat, lng, name }) {
   const position = [lat, lng];
   return (
-    <MapContainer
-      center={position}
-      zoom={18}
+    <div
       style={{
-        width: "1200px",
-        height: "400px",
+        maxWidth: "1200px",
+        width: "100%",
         margin: "0 auto",
       }}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position} icon={new Icon({ iconUrl: markerIcon, shadowUrl: markerShadow })}>
-        <Popup>{name}</Popup>
-      </Marker>
-    </MapContainer>
+      <MapContainer
+        center={position}
+        zoom={18}
+        style={{
+          width: "100%",
+          height: "400px",
+        }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker
+          position={position}
+          icon={new Icon({ iconUrl: markerIcon, shadowUrl: markerShadow })}
+        >
+          <Popup>{name}</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
